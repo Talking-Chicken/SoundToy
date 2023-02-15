a69da3fcacdb79bb8cf87cab8418a400b0e7d4b9
Leo Ji <sj3026@nyu.edu>
2023-02-14 21:10:18 -0500

    imported ReactOnBeat example

---
+++ Assets/Scripts/ReactOnBeat.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Beat;
+
+public class ReactOnBeat : MonoBehaviour {
+
+    [SerializeField]
+    Clock clock;
+
+    SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+    Beat.TickValue tickValue;
+
+    bool pleaseChange = false;
+
+    #region Delegate
+
+    private void OnEnable() {
+        clock.Beat += OnBeat;
+        clock.Eighth += OnBeat;
+    }
+    private void OnDisable() {
+        clock.Beat -= OnBeat;
+        clock.Eighth -= OnBeat;
+    }
+
+    void OnBeat(Beat.Args beatArgs) {
+        if(tickValue == beatArgs.BeatVal) {
+            ReactAction();
+        }
+    }
+
+    #endregion
+
+    private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    void ReactAction() {
+        Debug.Log("Hey!!");
+        pleaseChange = true;
+    }
+
+    void ChangeColor() {
+        pleaseChange = false;
+        spriteRenderer.color = new Color(Random.Range(0.0f, 1.0f),
+            Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+    }
+
+    private void Update() {
+        if (pleaseChange) {
+            ChangeColor();
+        }
+    }
+
+}
